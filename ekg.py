c8b7fdc464fe85379f1258412eab8917728b0f4b
d-niels <d.niels1998@gmail.com>
2022-11-28 22:16:48 -0600

    expanded load_ekg

--- ekg.py
+++ ekg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.io import loadmat
 
 def get_ekg_features_test() -> pd.DataFrame:
     from sklearn import datasets
@@ -15,15 +16,34 @@ def get_ekg_features_test() -> pd.DataFrame:
 
     return data
 
+def load_ekg(filename: str) -> tuple((np.ndarray, dict[str: None])):
+    """
+    Loads all 
+    """
+    # Load the ekg voltage data from the .mat file
+    ekg = loadmat(filename + '.mat')['val']
 
-def get_ekg_features(ekg: np.ndarray, features: list[str]) -> pd.DataFrame:
+    # Load the features from the .hea file
+    f = open(filename + '.hea', 'r')
+    features = {}
+    for line in f:
+        if line[0] == '#':
+            contents = line.split(' ')[1:]
+            features[contents[0]] = contents[1][:len(contents[1])-1]
+    return ekg, features
+
+def get_ekg_features(ekg: np.ndarray) -> pd.DataFrame:
     """
     Takes in an ekg and a list of features and returns them
     """
     pass
 
-def load_ekgs() -> tuple(np.ndarray, dict[str: _]):
+def load_ekgs() -> tuple((np.ndarray, dict[str: None])):
     """
     Loads all ekgs in an np.ndarray
     """
     pass
+
+if __name__ == '__main__':
+    ekg, features = load_ekg('training/chapman_shaoxing/g1/JS00001')
+    print(features)
