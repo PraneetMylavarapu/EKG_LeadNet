ea3ee09f3e36a483b3ccfa85edb0452ce562e94d
Venkatakrishnan <venkatakrishnanranganathan2023@u.northwestern.edu>
2022-11-30 17:33:18 -0600

    function added to remove baseline wander from all leads

--- ekg.py
+++ ekg.py
@@ -53,6 +53,36 @@ def load_ekg(filename: str) -> tuple((np.ndarray, dict[str: None])):
             features[contents[0]] = contents[1][:len(contents[1])-1]
     return ekg, features
 
+def remove_baseline_wander(signal):
+    """
+    Removes baseline wander from all leads, takes nd-array as input
+    """
+    proc_signal = np.ndarray((0, signal.shape[1]))
+    for x in signal:
+        ssds = np.zeros((3))
+
+        cur_lp = np.copy(x)
+        iterations = 0
+        while True:
+
+            lp, hp = pywt.dwt(cur_lp, "db4")
+
+            ssds = np.concatenate(([np.sum(hp ** 2)], ssds[:-1]))
+
+            if ssds[2] > ssds[1] and ssds[1] < ssds[0]:
+                break
+
+            cur_lp = lp[:]
+            iterations += 1
+
+        baseline = cur_lp[:]
+        for _ in range(iterations):
+            baseline = pywt.idwt(baseline, np.zeros((len(baseline))), "db4")
+        new = x - baseline[: len(x)]
+        proc_signal = np.vstack((proc_signal, new))
+        print(proc_signal.shape, signal.shape)
+    return proc_signal
+
 def get_ekg_features(ekg: np.ndarray) -> pd.DataFrame:
     """
     Takes in an ekg and a list of features and returns them
