1828c300149a4516249237223f53bd56e28aaaa3
d-niels <d.niels1998@gmail.com>
2022-11-30 17:44:34 -0600

    another

--- test_forest.py
+++ test_forest.py
@@ -4,7 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import pandas as pd
 import numpy as np
-from ekg import get_ekg_features, load_ekgs
+from ekg import get_ekg_features_test, load_ekgs
 
 ekgs, features = load_ekgs()
-ekgs[0][1]
+
+# Extract features from waveforms
+features2 = []
+for ekg in ekgs:
+    features2.append(get_ekg_features_test(ekg))
+features2 = pd.DataFrame(data=features2)
+features3 = features.join(features2)
