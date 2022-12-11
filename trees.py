b5332b505d0381a405c6e02f6c1bdae79221ca2e
d-niels <d.niels1998@gmail.com>
2022-12-11 12:41:17 -0600

    more bug fixes

--- trees.py
+++ trees.py
@@ -1,5 +1,4 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import pandas as pd
